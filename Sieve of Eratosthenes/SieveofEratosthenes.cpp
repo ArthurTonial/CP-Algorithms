5f84731ad09aa506e337da7a5d0a9fafbc9b6444
Tonitodos <arth.t.s@gmail.com>
2021-04-21 23:17:15 -0300

    Sieve of Eratosthenes

---
+++ Sieve of Eratosthenes/SieveofEratosthenes.cpp
@@ -0,0 +1,40 @@
+// this code was creator with the intention of 
+// understand how the Sieve of Eratosthenes works
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N 100
+int natural_numbers[N+1];
+
+// function that make a vector with the primes 
+// between 1 and n (parameter)
+vector<int> primes(long long n) {
+
+	vector<int> pr;
+
+	for (long long i = 2; i <= n; i++) {
+
+		if (natural_numbers[i] == 0 and i * i <= n) {	
+			
+			pr.push_back(i);
+
+			for (long long j = i * i; j <= n; j += i) {
+				natural_numbers[j] = 1;
+			}
+		}
+	}
+	return pr;
+}
+
+
+// says if the index i is prime (true or false)
+/*int n;
+vector<char> is_prime(n+1, true);
+is_prime[0] = is_prime[1] = false;
+for (int i = 2; i * i <= n; i++) {
+    if (is_prime[i] && (long long)i * i <= n) {
+        for (int j = i * i; j <= n; j += i)
+            is_prime[j] = false;
+    }
+}*/
